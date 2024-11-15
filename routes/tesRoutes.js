869960e04161af826b0b8fc73768fbb8bb27ac03
Collin <cdchamberlin.rva@gmail.com>
2024-11-14 22:44:13 -0500

    Updated code

--- routes/tesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/test", (req, res) => {
-    res.send("Hello World");
-});
-router.get;
-module.exports = router;
